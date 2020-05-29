eccc6b8722e5249483aa24c1d1eddffc8c0167d8
vZ <vz@vZs-iMac.local>
2020-05-29 04:53:54 -0300

    Working on the JS

--- scripts/rgb_picker.js
+++ scripts/rgb_picker.js
@@ -1,1 +1,15 @@
-alert('Hello, world!')
+var r = 0;
+var g = 0;
+var b = 0;
+
+var r_range = document.getElementById('r').value;
+var g_range = document.getElementById('g').value;
+var b_range = document.getElementById('b').value;
+
+console.log(r_range)
+console.log(g_range)
+console.log(b_range)
+
+window.addEventListener('load', (event) => {
+    console.log('Hello, world!');
+});
