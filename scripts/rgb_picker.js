32c512387feaa48ec67cf5222e68117c359fdff4
vzro <robertzero@msn.com>
2020-06-05 05:57:04 -0300

    Uploading working project; Reworked all files, but specially the JS.

--- scripts/rgb_picker.js
+++ scripts/rgb_picker.js
@@ -1,3 +1,27 @@
-window.addEventListener('load', (event) => {
-    console.log('Hello, world!');
+var rgb = {'r':'0', 'g':'0', 'b':'0'};
+
+window.addEventListener('load', () => {
+    getSliders('.slider');
 });
+
+function getSliders (sliderClass) {
+    r = g = b = 0;
+    document.querySelectorAll(sliderClass)
+    .forEach(child => child.addEventListener('input', color => {
+        let colorID = color.target.attributes.id.value;
+        let colorValue = color.target.value;
+        setColor(colorID, colorValue, r, g, b);
+    }))}
+
+function setColor(colorID, colorValue, r, g, b) {
+    switch (colorID) {
+        case 'r': rgb.r = colorValue; r = colorValue; break;
+        case 'g': rgb.g = colorValue; g = colorValue; break;
+        case 'b': rgb.b = colorValue; b = colorValue; break;
+    }
+    let rgbBoxColor = `rgb(${r}, ${g}, ${b})`;
+    let rgbBGColor = `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`;
+    document.querySelector(`.${colorID}`).attributes.value.textContent = colorValue;
+    document.querySelector(`#${colorID}ColorBox`).style.backgroundColor = rgbBoxColor;
+    document.querySelector(`#container`).style.backgroundColor=`${rgbBGColor}`;
+}
